8606c7e73cc7e33290468fe4167cc406843ac607
Wisani Chauke <wiserowens@gmail.com>
2024-08-18 21:46:48 +0200

    it works!

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const UserProfile = (props) => {
   return (
     <div>
-      <h2>{Alice}</h2>
-      <p>Age: {25}</p>
-      <p>Bio: {"Loves hiking and photography"}</p>
+      <h2>{props.name}</h2>
+      <p>Age: {props.age}</p>
+      <p>Bio: {props.bio}</p>
     </div>
   );
 };
