fbd307dcb0ca99d1e7acf6dd664c2cf747afc439
Wisani Chauke <wiserowens@gmail.com>
2024-08-18 21:43:59 +0200

    Let's see

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,9 +3,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './Header';
+import Header from './Header'
 import MainContent from './MainContent'
-import Footer from './Footer';
+import Footer from './Footer'
+import UserProfile from './components/UserProfile'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -36,6 +37,8 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+
     </>
   )
 }
---
+++ alx-react-app/src/UserProfile.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const UserProfile = (props) => {
+  return (
+    <div>
+      <h2>{Alice}</h2>
+      <p>Age: {25}</p>
+      <p>Bio: {"Loves hiking and photography"}</p>
+    </div>
+  );
+};
+
+export default UserProfile;
