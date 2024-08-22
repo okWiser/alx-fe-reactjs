779917f00548370899b45b2928a61d6dd7f52d45
Wisani Chauke <wiserowens@gmail.com>
2024-08-22 12:19:25 +0200

    USerProfile

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React from 'react'
+import React, { useContext } from 'react';
+import UserContext from './components/UserContext'
 
 const UserProfile = (props) => {
+  const { user } = useContext(UserContext);
+
   return (
     <div>
       <h2>{props.name}</h2>
