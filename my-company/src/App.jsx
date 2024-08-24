f3af71cb136a3b368ddc30039234b7847b63c500
Wisani Chauke <wiserowens@gmail.com>
2024-08-24 10:56:44 +0200

    app jsx updatwe

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,14 +1,23 @@
+// src/App.jsx
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'; // Import necessary components
+import Headerbar from './HeaderBar'; // Make sure to import your components
+import Home from './Home';
+import About from './About';
+import Services from './Services';
+import Contact from './Contact';
 
-function App() {
-    return (
-        <div>
-            <About />
-            <Services />
-            <Home />
-            <Contact />
-        </div>
-    );
+export default function App() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <Headerbar />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/about" exact component={About} />
+          <Route path="/" exact component={Home} /> {/* Use "/" for the root path */}
+          {/* Add more routes as needed */}
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
-
-export default App;
