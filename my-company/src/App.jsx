ea16e62d954b3e94bb5d540f1756b3ddae3d6e7f
Wisani Chauke <wiserowens@gmail.com>
2024-08-24 10:53:51 +0200

    app jsx

---
+++ my-company/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <About />
+            <Services />
+            <Home />
+            <Contact />
+        </div>
+    );
+}
+
+export default App;
