2ad16f4345c53292b001d4eca2bd22bad7b08359
Wisani Chauke <wiserowens@gmail.com>
2024-08-24 11:22:08 +0200

    lest see

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,17 +1,37 @@
 // src/App.jsx
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'; // Import necessary components
-import Headerbar from './HeaderBar'; // Make sure to import your components
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Headerbar from './HeaderBar';
 import Home from './Home';
 import About from './About';
 import Services from './Services';
 import Contact from './Contact';
 import Routes from './Routes';
+import Navbar from './Navbar'; 
+
+export default function App() {
+return (
+    <div style={{ textAlign: 'center' }}>
+        <Navbar />
+        <BrowserRouter>
+            <Switch>
+                <Route path="/about" exact component={About} />{/About' component={About} */}
+                <Route path="/home" exact component={Home} /> {/Home' component={Home} */}
+                <Route path="/services" exact component={Services} /> {/Services' component={Services} */}
+                <Route path="/contact" exact component={Contact} /> {/Contact' component={Contact} */}
+            </Switch>
+        </BrowserRouter>
+    </div>
+);
+}
+
+
+
 
 ["Routes", "element"]
 // <BrowserRouter>
 
-<Route path="/" component={Routes} />
+<Route path="/" component={Routes} /> 
 export default function App() {
 }
     return (
