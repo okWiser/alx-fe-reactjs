366c75a35963163d9e8ed1bd31ca30c16b973109
Wisani Chauke <wiserowens@gmail.com>
2024-08-26 21:54:12 +0200

    sure

--- recipe-sharing-app/src/components/EditRecipeForm.jsx
+++ recipe-sharing-app/src/components/EditRecipeForm.jsx
@@ -2,34 +2,34 @@ import React, { useState } from 'react';
 import useRecipeStore from './recipeStore';
 
 function EditRecipeForm({ recipeId }) {
-  const [editedRecipe, setEditedRecipe] = useState({});
-  const updateRecipe = useRecipeStore((state) => state.updateRecipe);
-  const recipeToEdit = useRecipeStore((state) =>
-    state.recipes.find((recipe) => recipe.id === recipeId)
-  );
+    const [editedRecipe, setEditedRecipe] = useState({});
+    const updateRecipe = useRecipeStore((state) => state.updateRecipe);
+    const recipeToEdit = useRecipeStore((state) =>
+        state.recipes.find((recipe) => recipe.id === recipeId)
+    );
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setEditedRecipe((prev) => ({ ...prev, [name]: value }));
-  };
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setEditedRecipe((prev) => ({ ...prev, [name]: value }));
+    };
 
-  const handleSave = () => {
-    // Update the recipe in the store
-    updateRecipe({ ...recipeToEdit, ...editedRecipe });
-  };
+    const handleSave = () => {
+        updateRecipe({ ...recipeToEdit, ...editedRecipe });
+    };
 
-  return (
-    <div>
-      <input
-        type="text"
-        name="title"
-        value={editedRecipe.title || recipeToEdit.title}
-        onChange={handleInputChange}
-      />
-      {/* Other input fields for ingredients, instructions, etc. */}
-      <button onClick={handleSave}>Save</button>
-    </div>
-  );
+    return (
+        <div>
+            <form onSubmit={event => event.preventDefault()}>
+                <input
+                    type="text"
+                    name="description"
+                    value={editedRecipe.description || recipeToEdit.description}
+                    onChange={handleInputChange}
+                />
+                <button onClick={handleSave}>Save</button>
+            </form>
+        </div>
+    );
 }
 
 export default EditRecipeForm;
