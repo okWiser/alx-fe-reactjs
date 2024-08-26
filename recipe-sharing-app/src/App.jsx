af79e2f9a57f252deca9e3e90ec565c8cf998754
Wisani Chauke <wiserowens@gmail.com>
2024-08-26 21:08:53 +0200

    add

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import RecipeList from './RecipeList';
-import AddRecipeForm from './AddRecipeForm';
+import AddRecipeForm from './addRecipeForm';
 
 function App() {
   const [count, setCount] = useState(0)
