191b5ae8a0d4c9af6be75b84d5a09ffa4cb5c79e
Wisani Chauke <wiserowens@gmail.com>
2024-08-27 11:17:21 +0200

    link added

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,19 +1,21 @@
 import { useRecipeStore } from './recipeStore';
-
+import { Link } from 'react-router-dom';
 
 export function RecipeList() {
    const recipes = useRecipeStore(state => state.recipes);
 
    return (
-     <div>
-       {recipes.map(recipe => (
-         <div key={recipe.id}>
-           <h3>{recipe.title}</h3>
-           <p>{recipe.description}</p>
-         </div>
-       ))}
-     </div>
-   );
+    <div>
+      {recipes.map(recipe => (
+        <div key={recipe.id}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+          <Link to={`/recipes/${recipe.id}`}>View Recipe</Link>
+        </div>
+      ))}
+    </div>
+  );
  };
+
 
 
