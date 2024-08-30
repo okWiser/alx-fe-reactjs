f1181849df824f83a6f68ae82e19ac17c4b0912c
Wisani Chauke <wiserowens@gmail.com>
2024-08-30 21:02:53 +0200

    profile

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import { Outlet, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import ProfileDetails from './ProfileDetails';
+import ProfileSettings from './ProfileSettings';
 
 function Profile() {
-  return (
-    <div>
-      <h1>Profile</h1>
-      <nav>
-        <ul>
-          <li><Link to="details">Profile Details</Link></li>
-          <li><Link to="settings">Profile Settings</Link></li>
-        </ul>
-      </nav>
-      <Outlet />
-    </div>
-  );
+    return (
+        <div>
+            <h1>Profile</h1>
+            <nav>
+                <ul>
+                    <li><Link to="details">Profile Details</Link></li>
+                    <li><Link to="settings">Profile Settings</Link></li>
+                </ul>
+            </nav>
+            <Routes>
+                <Route path="details" element={<ProfileDetails />} />
+                <Route path="settings" element={<ProfileSettings />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default Profile;
