75f25d9c03eb36ef880da6a8ae428c678b55f1ba
Wisani Chauke <wiserowens@gmail.com>
2024-08-30 20:37:22 +0200

    src

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import PostsComponent from './components/PostsComponent';
+import PostsComponent from './PostsComponent';
 
 const queryClient = new QueryClient();
 
