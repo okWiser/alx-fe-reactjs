f47cff95e06f4ab57a4d083d6a6790df3b22904b
Wisani Chauke <wiserowens@gmail.com>
2024-08-30 20:29:29 +0200

    postscomponents

---
+++ react-query-demo/src/components/postsComponent.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+const fetchPosts = async () => {
+  const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts');
+  return data;
+};
+
+function PostsComponent() {
+  const { data, error, isLoading } = useQuery('posts', fetchPosts);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      <ul>
+        {data.map(post => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default PostsComponent;
