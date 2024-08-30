8450012eed7d0788c79a12a619a665e198289ed3
Wisani Chauke <wiserowens@gmail.com>
2024-08-30 18:35:26 +0200

    all set!

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -4,6 +4,17 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import React from 'react';
 import RegistrationForm from './components/RegistrationForm';
+import React from 'react';
+import FormikForm from './components/FormikForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Register</h1>
+      <FormikForm />
+    </div>
+  );
+}
 
 function App() {
   return (
