d459a7a3781aa0a95b7d08a6f08e73ea9cae29e6
Wisani Chauke <wiserowens@gmail.com>
2024-09-08 01:38:55 +0200

    rresponsive stykling

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const UserProfile = () => {
   return (
-    <div className="bg-gray-100 p-4 sm:p-8 max-w-xs sm:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+    <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
       <img
         src="your-image-url.jpg"
         alt="User Profile"
