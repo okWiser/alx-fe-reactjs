76e5030464fdc053ce4243a996ea93789a1daf6f
Wisani Chauke <wiserowens@gmail.com>
2024-09-11 21:01:30 +0200

    home page

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -2,9 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import HomePage from './components/Homepage'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  
+
+const App = () => {
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
+};
 
   return (
     <>
