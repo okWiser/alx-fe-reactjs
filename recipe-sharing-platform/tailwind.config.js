cc87410956c5bfface646d382b04cc63a02eceec
Wisani <wiserowens@gmail.com>
2024-09-22 20:28:08 +0200

    tailwind

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,12 +1,10 @@
-module.exports = {
-    purge: {
-      enabled: true,
-      content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-    },
-    darkMode: false,
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
-  
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false,
+}
